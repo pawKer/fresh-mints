47acf5616f43f3d4a07f2c296e041c6581d1ed89
pawKer <rares.rck96@yahoo.ro>
2022-03-13 16:49:48 +0000

    Added error handling for discord api errors

--- src/logic/check-and-send-updates.ts
+++ src/logic/check-and-send-updates.ts
@@ -1,6 +1,7 @@
 import { TextChannel } from "discord.js";
 import { DiscordClient } from "../../@types/bot";
 import { AddressData } from "../../@types/bot/ServerDataDTO";
+import { getErrorEmbed } from "../embeds/embeds";
 import { getMintEmbeds } from "./get-mint-embeds";
 import { getMintsForAddress } from "./get-mints-for-address";
 
@@ -14,31 +15,39 @@ const checkAndSendUpdates = async (
   infoChannel?: TextChannel,
   trackOpenseaBuys?: boolean
 ): Promise<string | undefined> => {
-  const mintCountResp = await getMintsForAddress(
-    client,
-    address,
-    data,
-    minutesToCheck,
-    serverId,
-    infoChannel,
-    data.isContract
-  );
+  try {
+    const mintCountResp = await getMintsForAddress(
+      client,
+      address,
+      data,
+      minutesToCheck,
+      serverId,
+      infoChannel,
+      data.isContract
+    );
 
-  if (!mintCountResp) return;
+    if (!mintCountResp) return;
 
-  const embedsToSend = getMintEmbeds(
-    mintCountResp,
-    minutesToCheck,
-    trackOpenseaBuys,
-    mintCountResp.forAddressData.isContract
-  );
+    const embedsToSend = getMintEmbeds(
+      mintCountResp,
+      minutesToCheck,
+      trackOpenseaBuys,
+      mintCountResp.forAddressData.isContract
+    );
 
-  if (embedsToSend.length > 0) {
-    for (const embed of embedsToSend) {
-      await alertChannel.send({ embeds: [embed] });
+    if (embedsToSend.length > 0) {
+      for (const embed of embedsToSend) {
+        await alertChannel.send({ embeds: [embed] });
+      }
+      return data.name;
     }
-    return data.name;
+    return;
+  } catch (e) {
+    console.error(`[${serverId}] [${address}] BOT_ERROR`, e);
+    infoChannel &&
+      infoChannel.send({
+        embeds: [getErrorEmbed(data.name, address, e.message, minutesToCheck)],
+      });
   }
-  return;
 };
 export { checkAndSendUpdates };
